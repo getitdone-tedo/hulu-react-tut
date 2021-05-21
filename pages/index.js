1578c5b0c60cf68a09931f51aa8b2a2ee64a6f2b
Tedo <getitdonetedo@gmail.com>
2021-05-21 10:10:26 -0700

    the app is fully working

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/header'
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -11,8 +14,28 @@ export default function Home() {
 
       {/* header */}
       <Header />
+
       {/* navbar */}
+      <Navbar />
+
       {/* results */}
+      <Results results={results} />
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
